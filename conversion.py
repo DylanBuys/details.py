9d835d6ecb34df09ba783863bd12c596f2678d7b
DylanBuys <buysdylan17@gmail.com>
2025-06-17 22:00:33 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,10 @@
+#declare the following variables: num1=99.3, num2=23, num3=150, strting1="100"
+num1 = 99.3
+num2 = 23
+num3 = 150
+strting1 = "100"
+#convert num1 to an integer, num2 to a float, num3 to a string, and strting1 to an integer
+num1_int = int(num1)
+num2_float = float(num2)
+num3_str = str(num3)
+strting1_int = int(strting1)
